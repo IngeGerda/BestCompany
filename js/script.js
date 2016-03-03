7739fba1d12c7202f7d0c6bfeb6b30565173cc12
IngeGerda Lungu <IngaGerdaL@gmail.com>
2016-03-03 18:28:08 +0200

    fixed nav,portfolio modals

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 /**
  * Created by IngeGerda on 19.02.2016.
  */
+//******Scroll*****//
+jQuery(document).ready(function($) {
+    $(".scroll").click(function(event){
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+    });
+});
